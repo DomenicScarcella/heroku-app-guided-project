1f9068bd6afef22e992a17be8b788ce82f35f176
Domenic Scarcella <ls41513@yahoo.com>
2021-04-08 15:27:54 -0400

    copied from Lambda repo to replace mine

--- index.js
+++ index.js
@@ -1,24 +1,27 @@
-const dotenv = require('dotenv').config()
-const express = require('express');
-const cors = require('cors');
-const app = express();
-const port = process.env.PORT || 9000;
-const path = require('path');
+const dotenv = require("dotenv").config()
+const express = require("express")
+const cors = require("cors")
+const app = express()
+const port = process.env.PORT || 9000
+const path = require("path")
 
-app.use(cors());
-app.use(express.json());
-app.use(express.static(path.join(__dirname, 'client/build')));
+app.use(cors())
+app.use(express.json())
+app.use(express.static(path.join(__dirname,"client/build")))
 
-app.use('/api/', (_, res) => {
-    res.json({data: 'API is accounted for'});
-});
+app.use("/api/",(_,res)=>{
+    res.json({data:"API is accounted for"})
+})
 
-app.listen(port, () =>{
-    console.log(`Server running on ${port}`);
-});
+app.listen(port,()=>{
+    console.log(`Server is alive on port ${port}`)
+})
 
-// console.log('It works!');
-// console.log(__dirname);
-// console.log(__filename);
-// console.log(process.env.USERNAME);
-// console.log(process.env.PORT);
+// console.log("Its alive!!!!!!")
+// console.log(__dirname)
+// console.log(__filename)
+// console.log(process.env.USERNAME)//mac USER
+// console.log(process.env.PORT)
+// console.log(process.env.FOOD)
+// console.log(process.env.REALLY)
+// console.log(process.env.GREET)
